55a3835e0329d65c0622e81235c73eb40f0c0285
Jessica Lombera <jesstlombera@gmail.com>
2015-03-11 18:39:35 -0700

    Fleshed out the function playBestOf.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -36,57 +36,86 @@ function getComputerMove(move) {
     return move || randomPlay;
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+    function getWinner(playerMove,computerMove) {
+        var winner;
+        // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+        // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+        // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+        
+        if (playerMove == computerMove) {
+            winner = "tie";
+        }
+
+        if (playerMove == "scissors" && computerMove == "paper") {
+            winner = "player"    
+        }
+
+        if (playerMove == "scissors" && computerMove == "rock") {
+            winner = "computer"
+        }
+
+        if (playerMove == "paper" && computerMove == "scissors") {
+            winner = "computer"
+        }
+
+
+        if (playerMove == "rock" && computerMove == "scissors") {
+            winner = "player"
+        }
+
+        if (playerMove == "paper" && computerMove == "rock") {
+            winner = "player"
+        }
+
+        if (playerMove == "rock" && computerMove == "paper") {
+            winner = "computer"
+        }
+
+
+        return winner;
+    }
+
+function playBestOf(limit) {
+    var countWinsByPlayer = 0;
+    var countWinsByComputer = 0;
+
+console.log("Playing the game til best of " + limit);
+
+    while(countWinsByPlayer < limit && countWinsByComputer < limit) {
+          
+       
+        var playerChoice;
+        playerChoice = getInput();
+        console.log("The player chose " + playerChoice);
     
-    if (playerMove == "scissors" && computerMove == "scissors") {
-        winner = "tie";
-    }
-
-    if (playerMove == "scissors" && computerMove == "paper") {
-        winner = "player"
-    }
-
-    if (playerMove == "scissors" && computerMove == "rock") {
-        winner = "computer"
-    }
-
-    if (playerMove == "paper" && computerMove == "scissors") {
-        winner = "comouter"
-    }
-
-    if (playerMove == "paper" && computerMove == "paper") {
-        winner = "tie"
-    }
-
-    if (playerMove == "rock" && computerMove == "scissors") {
-        winner = "player"
-    }
-
-    if (playerMove == "paper" && computerMove == "rock") {
-        winner = "player"
-    }
-
-    if (playerMove == "rock" && computerMove == "paper") {
-        winner = "computer"
-    }
-
-    if (playerMove == "rock" && computerMove == "rock") {
-        winner = "tie"
-    }
-
-    return winner;
-}
-
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+        var computerChoice;
+        computerChoice = randomPlay();
+        console.log("The computer chose " + computerChoice);
     
-    return [playerWins, computerWins];
-}
+        var finalWinner;
+        finalWinner = getWinner(playerChoice, computerChoice);
+        console.log("The winner is " + finalWinner);
+            if (finalWinner == "player") {
+                countWinsByPlayer++;
+            }  else if (finalWinner == "computer") {
+                countWinsByComputer++;
+            }
+     }
+
+        if (countWinsByPlayer > countWinsByComputer) {
+            console.log("The human Won!");
+        } else {
+            console.log("The robot won!");
+        }
+      
+  }
+  
+ 
+playBestOf(3);
+
+
+
+
+
+
 
